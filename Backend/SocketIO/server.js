f45dba398d56bd193fb027c0ea54b9b7a74b1209
ANIKET SINGH <125822570+aniketsingh-214@users.noreply.github.com>
2025-09-13 14:46:21 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://real-time-chat-app-2bcv.vercel.app", 
+    origin: "https://realtimechatapp214.vercel.app", 
     methods: ["GET", "POST"],
     credentials: true
   },
